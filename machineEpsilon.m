d9e699585ce5be3eb1723d1ff9d6318553639de0
Won Bin Choi <dc07650@naver.com>
2020-12-16 20:29:27 +0900

    Update machineEpsilon.m

--- machineEpsilon.m
+++ machineEpsilon.m
@@ -1,5 +1,13 @@
+%% Numerical Analysis MATLAB
+% Author: Won Bin Choi (B3)
+% E-mail: dc07650@naver.com
+% Organization: Sogang University(Korea, Republic of), Mechanical Engineering
+% Date: September 10, 2020
+
+%% Initilizae
 clear all; close all; clc;
-a = 1;
+a = 1; % Set random initial value
+%% Main Loop
 while true
    disp(a);
    a = a/2;
@@ -7,4 +15,5 @@ while true
        break
    end
 end
+%% Display
 disp("Terminate Program")
