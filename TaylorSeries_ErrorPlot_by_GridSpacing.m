b2dbff9e8a32a48f733ab659edff21d46cde065d
Won Bin Choi <dc07650@naver.com>
2020-12-16 20:33:26 +0900

    Update TaylorSeries_ErrorPlot_by_GridSpacing.m

--- TaylorSeries_ErrorPlot_by_GridSpacing.m
+++ TaylorSeries_ErrorPlot_by_GridSpacing.m
@@ -1,12 +1,18 @@
-clear all; close all; clc
+%% Numerical Analysis MATLAB
+% Author: Won Bin Choi (B3)
+% E-mail: dc07650@naver.com
+% Organization: Sogang University(Korea, Republic of), Mechanical Engineering
+% Date: September 10, 2020
 
-syms x
+%% Initilize
+clear all; close all; clc
+syms x;
 % f(x) = sin(x);
-func = input("Input function: ", 's');
+func = input("Input function: ", 's'); % Set target function
 f(x) = str2sym(func);
 df(x) = diff(f(x));
-n = 3;
-
+n = 3; % Number of iteration for reduction of grid spacing, h
+%% Main Loop
 while true
     disp("Extrapolation Plot")
     disp(f)
@@ -15,12 +21,13 @@ while true
     
     xi = linspace(0,2*pi, n);
     dphi = zeros(1,n);
-
+    %% Obtain all approximated values using 2 point centered
     for i = 1:1:length(xi)
         dphi(i) = (f(h+xi(i)) - f(-1*h+xi(i)))/(2*h);
     end
-    
+    %% Actual Value
     y = df(xi);
+    %% Plot
     hold on;
     plot(xi, y);
     plot(xi, dphi, "--");
@@ -29,7 +36,7 @@ while true
     legend ('Real df(x)', 'Approximated df(x)');
     title("Error by Centered Difference");
     hold off;
-    
+    %% Termination branch
     comm = input("\nTerminate? (y/n): ", 's');
     if (comm == 'y')||(comm == 'Y')
         clear all; close all;
