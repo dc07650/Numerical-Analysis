8a86386fa432663b0be5f18db7481f0fb5972a89
Won Bin Choi <dc07650@naver.com>
2020-12-16 20:20:13 +0900

    Update newtonRaphsonMethod.m
    Added comments

--- newtonRaphsonMethod.m
+++ newtonRaphsonMethod.m
@@ -1,15 +1,20 @@
-clear all; close all; clc;
+%% Numerical Analysis MATLAB
+% Author: Choi Won Bin (B3)
+% E-mail: dc07650@naver.com
+% Organization: Sogang University(Korea, Republic of), Mechanical Engineering
+% Date: September 10, 2020
 
-syms f;
-syms x;
+%% Initialize
+clear all; close all; clc;
+syms f; syms x;
+%% Set parameters
 f(x) = ((9.8*68.1)/x)*(1-exp((-x*10)/(68.1)))-40
 %f(x) = 2*x + 1
 df(x) = diff(f(x))
-es = 0.00001;
-
-x0 = input("Input starting point: ");
+es = 0.00001; % Allowed error size
+x0 = input("Input starting point: "); %% Derivation point
 xi = x0;
-
+%% Main function (Newton Raphson Method)
 while true
    xii = xi - (f(xi))/(df(xi));
    if abs(f(xii)) < es
@@ -18,6 +23,6 @@ while true
        xi = xii;
    end
 end
-
+%% Output
 disp("The approximated root utilizing the Newton Raphson's Method is:")
 disp(double(xii));
