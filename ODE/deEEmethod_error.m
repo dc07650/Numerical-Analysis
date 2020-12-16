911a2bca4f882f9640988f308f895c7406847d29
Won Bin Choi <dc07650@naver.com>
2020-12-16 20:46:26 +0900

    ODE file upload

---
+++ ODE/deEEmethod_error.m
@@ -0,0 +1,44 @@
+%% Numerical Analysis MATLAB
+% Author: Won Bin Choi (B3)
+% E-mail: dc07650@naver.com
+% Organization: Sogang University(Korea, Republic of), Mechanical Engineering
+% Date: November 12, 2020
+%% Initialize
+close all; clear all; clc;
+syms y; syms x;
+lambda = 2; n = 1000;
+lset = 0; rset = 2;
+y(x) = exp(lambda*x);
+%% Compute
+h = (rset-lset)/n;
+t = lset:h:rset;
+yn = zeros(1,n);
+yn(1) = double(y(lset));
+for i = 1:n
+    yn(i+1) = (1+lambda*h)*yn(i);
+end
+%% Compute 2
+h = (rset-lset)/(2*n);
+t2 = lset:h:rset;
+yn2 = zeros(1,2*n);
+yn2(1) = double(y(lset));
+for i = 1:2*n
+    yn2(i+1) = (1+lambda*h)*yn2(i);
+end
+%% Plot
+hold on;
+plot(t,y(t));
+plot(t,yn);
+plot(t2,yn2);
+grid on; xlim([lset rset]); ylim([0 ceil(max(double(y(t)))/10)*10]);
+legend("Exact", "Approximate", "Approximate (Double Grid)");
+hold off;
+%% Error
+avgError = zeros(2,1);
+avgError(1) = mean(yn - double(y(t)));
+avgError(2) = mean(yn2 - double(y(t2)));
+
+fprintf("Error Reduction Rate of Explicit Euler Method\n");
+fprintf("n = %d:\th = %.3f, mean error = %.3f\n",n, (rset-lset)/n, avgError(1));
+fprintf("n = %d:\th = %.3f, mean error = %.3f\n",2*n, (rset-lset)/(2*n), avgError(2));
+fprintf("Reduction Rate: %f%%\n",avgError(2)/avgError(1)*100);
---
+++ ODE/deRKmethod_matrix.m
@@ -0,0 +1,58 @@
+%% Numerical Analysis MATLAB
+% Author: Won Bin Choi (B3)
+% E-mail: dc07650@naver.com
+% Organization: Sogang University(Korea, Republic of), Mechanical Engineering
+% Date: November 12, 2020
+
+%% Initilize
+clear all; close all; clc;
+lset = 0; rset = 5;
+init_x = [1; 0];
+n = 1000;
+h = (rset-lset)/n;
+tn = lset:h:rset;
+%% Main
+[t x]  = ode45(@mck, tn, init_x');
+
+%% Numerical
+a = [0 1; -25 -5];
+yn = zeros(2,n);
+yn(:,1) = init_x;
+for i = 1:n
+    yn(:,i+1) = (eye(2,2)+ a*h + a*a*h*h/2)*yn(:,i);
+end
+
+%% Graph
+Position = x(:, 1);
+Velocity = x(:, 2);
+Position_app = yn(1,:);
+Velocity_app = yn(2,:);
+figure(1); hold on; grid on;
+plot(t, Position);
+plot(tn, Position_app);
+plot(t, Velocity);
+plot(tn, Velocity_app);
+xlabel('times(sec)');
+ylabel('response');
+legend('Position(m)', 'Position(m)_{APP}', 'Velocity(m/s)', 'Velocity(m/s)_{APP}','location', 'southeast')
+
+%% Error
+avgError = zeros(2,1);
+avgError(1) = mean(Position_app - Position');
+avgError(2) = mean(Velocity_app - Velocity');
+
+fprintf("Error of Runge Kutta Method\n");
+fprintf("Position - n = %d:\th = %.3f, mean error = %f\n",n, (rset-lset)/n, avgError(1));
+fprintf("Velocity - n = %d:\th = %.3f, mean error = %f\n",n, (rset-lset)/n, avgError(2));
+
+%% Function
+function dx = mck(t,x)
+    m = 1;
+    c = 5;
+    k = 25;
+    u = 0;
+    dx = zeros(2,1);
+    
+    dx(1) = x(2);
+    dx(2) = -c/m * x(2) -k/m * x(1) +1/m * u;
+end
